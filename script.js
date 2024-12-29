f4cf4c9e55f56bebac7087bc48584c83f65bb1f1
Dor Lazarov <lazamal@gmail.com>
2024-12-29 15:56:40 +0200

    p

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// // Index fade in animation
-
-// window.onload = () => {
-//     const introText = document.getElementById('intro-container')
-//     introText.style.opacity = 0
-//     setTimeout(() => {
-//         introText.style.transition = 'opacity 3s'
-//         introText.style.opacity = 1
-//     }, 200)
-// }
-
 // creating the header component
 
 const headerDiv = document.createElement('header')
