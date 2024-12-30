f54708ea3189015d87976850762b14c91f46459d
Dor Lazarov <lazamal@gmail.com>
2024-12-30 21:49:45 +0200

    responsive navbar

--- script.js
+++ script.js
@@ -39,15 +39,38 @@ footerDiv.innerHTML = ` <h2>Get in Touch</h2>
             <a href="https://www.linkedin.com/in/dor-lazarov-65601287/" style="border:none; text-decoration:none;"
                 class="btn">LinkedIn</a>`
 
-// inserting the header and footer components
+// creating the responsive menu
+const hamMenu = document.createElement('nav')
+hamMenu.innerHTML = `<div class="ham-menu">
+<span></span>
+<span></span>
+<span></span>
+</div>`
+hamMenu.id = 'hamMenu'
 
+const offScreenMenu = document.createElement('div')
+offScreenMenu.innerHTML = `   <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="Products1.html">Hachaluz League</a></li>
+        <li><a href="Products2.html">Youth-movement CMS</a></li>
+         <li><a href="articles.html">Articles</a></li>
+         <li><a href="contact.html">Contact</a></li>
+      </ul>`
+offScreenMenu.className = 'off-screen-menu'
+
+// inserting the header and footer components
+document.body.prepend(offScreenMenu)
 document.body.prepend(headerDiv)
+document.body.prepend(hamMenu)
 document.body.append(footerDiv)
+window.onload = checkWindow
+window.onresize = checkWindow
 
 // checking the window size
 
 function checkWindow() {
-    screenWidth = screen.availWidth
+    screenWidth = screen.width
     if (screenWidth > 900) {
         console.log('desktop')
     }
@@ -55,8 +78,9 @@ function checkWindow() {
         console.log('mobile')
     }
 }
-window.onresize = checkWindow
 
-// creating the responsive menu
-const hamMenu = document.createElement('nav')
-hamMenu.className = 'ham-menu'
+// clicking the mobile navbar
+hamMenu.addEventListener('click', function () {
+    hamMenu.classList.toggle('active')
+    offScreenMenu.classList.toggle('active')
+})
